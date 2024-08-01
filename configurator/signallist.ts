9b2aafb78f0b1f76b9fad13346b4983266d62276
ellenfel <ellfelfen@gmail.com>
2024-08-01 18:58:16 +0300

    updated mqtt.json

---
+++ configurator/signallist.ts
@@ -0,0 +1,27 @@
+"water_detector": Math.random() < 0.5, // Boolean
+"lid_switch": Math.random() < 0.5, // Boolean
+"pulse_a": Math.random() * 1000, // Random number between 0 and 1000
+"pulse_b": Math.random() * 1000, // Random number between 0 and 1000
+"pulse_420_ir_bc": Math.random() * 1000, // Random number between 0 and 1000
+"voltage_a": Math.random() * 100, // Random voltage between 0 and 100
+"voltage_b": Math.random() * 100, // Random voltage between 0 and 100
+"voltage_c": Math.random() * 100, // Random voltage between 0 and 100
+"current_a": Math.random() * 100, // Random current between 0 and 100
+"current_b": Math.random() * 100, // Random current between 0 and 100
+"current_c": Math.random() * 100, // Random current between 0 and 100
+"420_ir_a": Math.random() * 100, // Random value between 0 and 100
+"420_ir_b": Math.random() * 100, // Random value between 0 and 100
+"420_ir_c": Math.random() * 100, // Random value between 0 and 100
+"temp": Math.random() * 40, // Random temperature between 0 and 40
+"hum": Math.random() * 100, // Random humidity between 0 and 100
+"pressure": Math.random() * 1000, // Random pressure between 0 and 1000
+"thermocouple_a": Math.random() * 100, // Random value between 0 and 100
+"thermocouple_b": Math.random() * 100, // Random value between 0 and 100
+"thermocouple_c": Math.random() * 100, // Random value between 0 and 100
+"mppt_batary_voltage": Math.random() * 100, // Random voltage between 0 and 100
+"mppt_panel_power": Math.random() * 1000, // Random power between 0 and 1000
+"harvesting_battery_voltage": Math.random() * 100, // Random voltage between 0 and 100
+"harvesting_battery_power": Math.random() * 1000, // Random power between 0 and 1000
+"ipec_pddata_avg_pd_a": Math.random() * 100, // Random value between 0 and 100
+"ipec_pddata_avg_pd_b": Math.random() * 100, // Random value between 0 and 100
+"ipec_pddata_avg_pd_c": Math.random() * 100 // Random value between 0 and 100
