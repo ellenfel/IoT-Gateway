1074d10851918488ec7d10007f07ac70cf3de3e7
ellenfel <ellfelfen@gmail.com>
2024-08-12 19:21:17 +0300

    working version

--- docker/commands.sh
+++ docker/commands.sh
@@ -1,9 +1,15 @@
 newgrp docker
 
-docker run -it -p 1884:1884 thingsboard/tb-gw-mqtt-broker:latest #gw test
+#Show both running and stopped containers (-a, --all)
+docker ps -a
+docker ps -f status=running
 
-docker compose up
+docker compose up # .yml file present
 docker compose up -d # at background
+
+docker stop tb-gateway
+
+docker run -it -p 1884:1884 thingsboard/tb-gw-mqtt-broker:latest #gw test
 
 mosquitto -c mosquitto.conf -v
 
