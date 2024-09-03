1ab41368419cccf9a9653ae86ffd4de92f9dda7f
ellenfel <ellfelfen@gmail.com>
2024-09-03 16:32:39 +0300

    adding a new decoder for celluar sersor uc300

---
+++ scripts/app-parse/api-script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This script is designed to publish telemetry data to a ThingsBoard instance using its REST API. 
+# It automates the process of sending sensor or device data to the ThingsBoard platform, where it can be monitored, analyzed, and visualized in real-time. 
+# The script fetches data from connected devices or pre-defined sources, structures the data into JSON format, and then sends it as an HTTP POST request to the ThingsBoard server.
+# The key steps include authenticating with the ThingsBoard API using a device access token, preparing the telemetry data, 
+# and handling the HTTP communication with error checking and retries to ensure reliable data transmission.
+# This script can be adapted to various use cases, including IoT projects where multiple sensors or devices need to report data to a central monitoring platform.
+
+
+# MQTT Configuration
+MQTT_HOST="127.0.0.1"
+MQTT_PORT="1883"
+
+# Define MQTT Topics
+MQTT_TOPICS=(
+    "TXUC300"
+)
+
+# Define ThingsBoard URLs
+THINGSBOARD_URLS=(
+    "http://127.0.0.1:8080/api/v1/GAKmp8ApfAsagXg1fPch/telemetry"
+)
+
+# Path to the JavaScript decoding script
+DECODE_SCRIPT_PATH="/home/ellenfel/Desktop/repos/project-iot-gateway/scripts/app-template/decode.js"
+
+# Subscribe to MQTT topics
+for ((i=0; i<${#MQTT_TOPICS[@]}; i++)); do
+    mosquitto_sub -h $MQTT_HOST -p $MQTT_PORT -t ${MQTT_TOPICS[$i]} | while read -r raw_hex_message; do
+        # Call the JavaScript decoding script
+        decoded_message=$(node $DECODE_SCRIPT_PATH "$raw_hex_message")
+
+        # Execute curl command with the decoded message and corresponding ThingsBoard URL
+        curl -v -X POST ${THINGSBOARD_URLS[$i]} --header "Content-Type: application/json" --data "$decoded_message"
+    done &
+done
+
+# Wait for all background processes to finish
+wait
+
---
+++ scripts/app-parse/test_api-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Path to the JavaScript decoding script
+DECODE_SCRIPT_PATH="/home/ellenfel/Desktop/repos/project-iot-gateway/scripts/app-parse/decode.js"
+
+# Test input data
+TEST_INPUTS=(
+    "7ef4 8100 0b5e 4ac4 661d 0300 5500 0000 0309 0000 0b09 0000 1309 0000 1b09 0000 2309 0000 2b09 0000 3309 0000 3b09 0000 4309 0000 4b09 0000 5309 0000 5b09 0000 6309 0000 6b09 0000 7309 0000 7b09 0000 8309 0000 8b09 1801 9309 1801 9b09 1801 a309 0000 ab09 0000 b309 0000 bb09 0000 c309 0000 cb09 0000 d309 0000 db09 0000 7e"  # "Mocha Language Test"
+)
+
+# Run tests
+for ((i=0; i<${#TEST_INPUTS[@]}; i++)); do
+    test_input=${TEST_INPUTS[$i]}
+    decoded_output=$(node $DECODE_SCRIPT_PATH "$test_input")
+
+    echo $decoded_output
+done
